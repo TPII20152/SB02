bf306d2711d59209dcc29b5ee902b5f663fb573c
kevinkbc <c.kevinbarros@gmail.com>
2015-12-06 00:56:54 -0300

    classes para usar sqlite

---
+++ SistemaBancario/src/br/ufc/banco/database/SQLiteConnection.java
@@ -0,0 +1,32 @@
+package br.ufc.banco.database;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SQLiteConnection {
+ 
+	Connection connection;
+    public Connection getConnection() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            String dbURL = "jdbc:sqlite:product.db";
+            connection = DriverManager.getConnection(dbURL);
+            if (connection != null) {
+                System.out.println("Connected to the database");
+                DatabaseMetaData dm = (DatabaseMetaData) connection.getMetaData();
+                System.out.println("Driver name: " + dm.getDriverName());
+                System.out.println("Driver version: " + dm.getDriverVersion());
+                System.out.println("Product name: " + dm.getDatabaseProductName());
+                System.out.println("Product version: " + dm.getDatabaseProductVersion());
+                connection.close();
+            }
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return connection;
+    }
+}
